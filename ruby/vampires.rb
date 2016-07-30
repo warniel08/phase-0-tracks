70af8d6f00534b252137badb10cfd7181dd6606f
Warner Nielsen <bwn888@gmail.com>
2016-07-30 17:02:42 -0600

    Add questions to ruby file to ask the user, store in variables

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+puts "What is your name?"
+vampire_name = gets.chomp
+
+puts "How old are you?"
+vampire_age = gets.chomp
+
+puts "What year were you born?"
+vampire_birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance? (y/n)"
+health_insurance = gets.chomp
+
