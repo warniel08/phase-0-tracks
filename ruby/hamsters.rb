6f50873ac6d15301bded30def654ecd73f749657
Warner Nielsen <bwn888@gmail.com>
2016-08-02 17:28:33 -0600

    Change variable output to appropriate data type

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,7 +2,7 @@ puts "What is the hamster's name?"
 name = gets.chomp
 
 puts "How loud is your hamster? (Enter number between 1-10)"
-volume = gets.chomp
+volume = gets.chomp.to_i
 
 puts "What color is your hamster?"
 color = gets.chomp
@@ -12,6 +12,6 @@ adoption = gets.chomp
 
 puts "Approximately how old is your hamster?"
 age = nil
-age = gets.chomp
+age = gets.chomp.to_f
 
 p name, volume, color, adoption, age
