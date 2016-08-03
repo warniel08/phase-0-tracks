8cea7ea1a5408a6d1782d2c4a58a31912ff38848
Warner Nielsen <bwn888@gmail.com>
2016-08-02 18:15:12 -0600

    Change conditional statements to include true false for each variable, udpate logic statements so they're correct

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,15 +1,51 @@
-puts "What is your name?"
-vampire_name = gets.chomp
+print "What is your name? "
+name = gets.chomp
 
-puts "How old are you?"
-vampire_age = gets.chomp
+print "How old are you? "
+age = gets.chomp
 
-puts "What year were you born?"
-vampire_birth_year = gets.chomp
+print "What year were you born? "
+birth_year = gets.chomp
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+print "Our company cafeteria serves garlic bread. Should we order some for you? (y/n) "
 garlic_bread = gets.chomp
 
-puts "Would you like to enroll in the company's health insurance? (y/n)"
+print "Would you like to enroll in the company's health insurance? (y/n) "
 health_insurance = gets.chomp
 
+
+if age.to_i == (2016 - birth_year.to_i)
+	correct_age = true
+else
+	correct_age = false
+end
+
+if garlic_bread == "y"
+	wants_garlic = true
+else
+	wants_garlic = false
+end
+
+if health_insurance == "y"
+	wants_health = true
+else
+	wants_health = false
+end
+
+if name == ("Drake Cula" || "Tu Fang")
+	name_input = true
+else
+	name_input = false
+end
+
+if correct_age == true && wants_garlic == true && wants_health == true && name_input == true
+		puts "Definitely a vampire."
+	elsif correct_age == true && (wants_garlic == true || wants_health == true)
+		puts "Probably not a vampire."
+	elsif correct_age != true && wants_garlic != true && wants_health != true
+		puts "Almost certainly a vampire."
+	elsif correct_age != true && (wants_garlic != true || wants_health != true)
+		puts "Probably a vampire."
+	else
+		puts "Results inconclusive."
+end
