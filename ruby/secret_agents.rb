b502937946f8a916a5559f16ca54d8152911c433
Warner Nielsen <bwn888@gmail.com>
2016-08-03 19:08:59 -0600

    Add pseudcode to file for encrypt and decrypt methods

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,17 @@
+#encrypt method
+#define encrypt method
+#ask user for a string
+#store the string in a variable
+#advance each letter of the string one letter forward
+#keep space character a space character, assume lowercase
+#print the new string to the screen
+#end the method
+
+#decrypt method
+#define decrypt method
+#ask user for a string
+#store the string in a variable
+#reverse each letter of the string one letter backward
+#keep space character a space character, assume lowercase
+#print the new string to the screen
+#end the method
