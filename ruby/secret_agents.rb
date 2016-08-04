7d826bea21d3e739c6f0866e9f49a844c77e061e
Warner Nielsen <bwn888@gmail.com>
2016-08-03 20:04:53 -0600

    Add decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,8 +1,7 @@
 #encrypt method
-#set index variable
 #define encrypt method
-#ask user for a string
-#store the string in a variable
+#set index variable
+#create while loop that has index position less than string length
 #advance each letter of the string one letter forward
 #keep space character a space character, assume lowercase
 #print the new string to the screen
@@ -25,13 +24,43 @@ def encrypt(x)
 	while index < string.length
 		if string[index] == " "
 			index += 1
+		elsif string[index] == "z"
+			string[index] = "a"
+			index += 1
 		else
-		string[index] = string[index].next
-  		index += 1
-  	end
+			string[index] = string[index].next
+  			index += 1
+  		end
 	end
 	puts string
 end
 
-encrypt("one two")
+encrypt("")
+
+
+def decrypt(x)
+	index = 0
+	string = x
+	alpha = "abcdefghijklmnopqrstuvwxyz"
+	while index < string.length
+		if string[index] == " "
+			index += 1
+		else
+			string[index] = alpha[index-1]
+  			index += 1
+  		end
+	end
+	puts string
+end
+
+decrypt("bcd")
+
+
+#alpha's index position subtracting 1
+
+
+
+
+
+
 
