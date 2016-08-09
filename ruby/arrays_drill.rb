4c52885dc685d1911c6e5dfdd5cf41d3cda7d15d
Warner Nielsen <bwn888@gmail.com>
2016-08-09 14:01:18 -0600

    Add comments at beginning, clean up comments

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,5 +1,8 @@
+# Warner Nielsen & Christopher Mendoza
+# 5.2
 
-#create a method with three items
+
+# create a method with three items
 def build_array(item_one, item_two, item_three)
 	new_array2 = []
 	new_array2 << item_one
@@ -8,37 +11,43 @@ def build_array(item_one, item_two, item_three)
 	return new_array2
 end
 
-#create a method that takes array and item as parameter
+# create a method that takes array and item as parameter
 def add_to_array(array, item)
 	array << item
 	return array
 end
 
-
-#Initialize empty array
+# initialize empty array
 new_array = []
-#add five items
+
+# add five items
 new_array = ["apple", "train", "plane", "automobile"]
 new_array << "bear"
 p new_array
-#Delete the item at index 2
+
+# delete the item at index 2
 new_array.delete_at(2)
 p new_array
-#insert new item
+
+# insert new item
 new_array.insert(2, "wheel")
 p new_array
-#remove first item without referring to index number.
+
+# remove first item without referring to index number.
 new_array.delete("apple")
 p new_array
-#does array include certain item
+
+# does array include certain item
 if new_array.include?("wheel") == true
 	puts "Yes, it is in the array"
 else
 	puts "No, it is not in the array"
 end
-#initialize another array with items in it
+
+# initialize another array with items in it
 another_array = [1, 3, 4, 5]
-#add two arrays together and store in new variable
+
+# add two arrays together and store in new variable
 p join_array = new_array + another_array
 
 # Question 1 Release 2
