50e0f377d51927d29106cdf2d8b45bf7c58b2e7d
Warner Nielsen <bwn888@gmail.com>
2016-08-09 10:18:17 -0600

    Separate calls before and after to read code easier

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -11,7 +11,7 @@
 #	puts "Goodbye"}
 
 
-# hash
+puts "\nHash"
 house_paint = {
 	"living room" => "yellow",
 	kitchen: "blue",
@@ -19,17 +19,49 @@ house_paint = {
 	bedroom: "gray"
 }
 
-# array
-furniture = ["sofa", "desk", "dining table", "bed"]
+puts "Before .each call:"
+p house_paint
 
+puts "\nAfter .each call:"
 house_paint.each do |room, color|
 	puts "The #{room} will be painted #{color}!"
 end
 
-puts "\nThese are the pieces we own: " 
+puts "\nBefore .map call:"
+p house_paint
+
+puts "\nAfter .map call:"
+house_paint.map do |room, color|
+	color = "white"
+	p color
+end
+puts "\n.map call didn't permanently change anything:"
+p house_paint
+
+
+puts "\nArray"
+# Before .each call
+furniture = ["sofa", "desk", "dining table", "bed"]
+p furniture
+
+# After .each call
+puts "These are the pieces we own: " 
 furniture.each do |piece| 
 	puts "#{piece}"
 end
+
+# Before .map call
+p furniture
+
+# After .map call
+	puts "These are the pieces we own: " 
+	furniture.map do |piece| 
+		puts "#{piece}"
+end
+
+# .map call didn't permanently change anything
+p furniture
+
 
 
 
