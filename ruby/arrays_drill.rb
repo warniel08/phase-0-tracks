04457ea6eeb1d171726a7868450eee99b329b157
Warner Nielsen <bwn888@gmail.com>
2016-08-09 13:56:52 -0600

    Add methods

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,4 +1,20 @@
 
+#create a method with three items
+def build_array(item_one, item_two, item_three)
+	new_array2 = []
+	new_array2 << item_one
+	new_array2 << item_two
+	new_array2 << item_three
+	return new_array2
+end
+
+#create a method that takes array and item as parameter
+def add_to_array(array, item)
+	array << item
+	return array
+end
+
+
 #Initialize empty array
 new_array = []
 #add five items
@@ -24,4 +40,11 @@ end
 another_array = [1, 3, 4, 5]
 #add two arrays together and store in new variable
 p join_array = new_array + another_array
+
+# Question 1 Release 2
+p build_array("three", 1, true)
+
+# Question 2 Release 2
+p add_to_array([], "a")
+p add_to_array(['a','b', 'c', 1, 2], 3)
 
