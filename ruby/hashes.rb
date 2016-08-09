8f420b5cd15d96e917102b44f3a6d65ade424c43
Warner Nielsen <bwn888@gmail.com>
2016-08-09 15:22:16 -0600

    Finish case block, change output to screen after change in key:value pair

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -65,27 +65,52 @@ puts "	Enter 'years' to change number of years."
 puts "	Enter 'watch' to change if you watch HGTV or not."
 puts "	Enter 'none' if you don't want to update anything."
 
+puts "\n"
 choice = gets.chomp.downcase
 case choice
-when "name"
-	print "Please enter correct name: "
-	name_change = gets.chomp
-		user[:name] = name_change
-		puts "Thank you, #{name_change}."
-when "children"
-	print "Please enter correct number of children: "
-	children_change = gets.chomp
-		user[:children] = children_change
-		puts "Thank you, you entered #{children_change} children."
-when "theme"
-	print "Please enter correct decor theme: "
-	theme_change = gets.chomp
-		user[:theme] = theme_change
-		puts "Thank you, your new theme is: #{theme_change}."
-		
+	when "name"
+		print "\nPlease enter correct name: "
+		name_change = gets.chomp
+			user[:name] = name_change
+			puts "\nThank you, #{name_change}."
+	when "children"
+		print "\nPlease enter correct number of children: "
+		children_change = gets.chomp
+			user[:children] = children_change
+			puts "\nThank you, you entered #{children_change} children."
+	when "theme"
+		print "\nPlease enter correct decor theme: "
+		theme_change = gets.chomp
+			user[:theme] = theme_change
+			puts "\nThank you, your preferred decor theme is: #{theme_change}."
+		when "shade"
+			print "\nPlease change your favorite shade of green: "
+			shade_change = gets.chomp
+				user[:shade] = shade_change
+				puts "\nThank you, your favorite shade of green is: #{shade_change}."
+		when	"rooms"
+			print "\nPlease enter correct number of rooms in your home: "
+			rooms_change = gets.chomp
+				user[:rooms] = rooms_change
+				puts "\nThank you, the correct number of rooms in your home is: #{rooms_change}."
+		when "years"
+			print "\nPlease enter the correct number of years you've lived in your home: "
+			years_change = gets.chomp
+				user[:years] = years_change
+				puts "\nThank you, the correct number of years you've lived in your home is: #{years_change}."
+		when "watch"
+			print "\nPlease enter (y/n) if you watch HGTV: "
+			hgtv_change = gets.chomp
+				user[:hgtv] = hgtv_change
+				puts "\nThank you, for updating your HGTV response to: #{hgtv_change}."
+		when "none"
+			puts "\nThank you, you chose to leave your input the same."
 end
 
-p user
+puts "\n"
+user.each do |question, answer|
+	puts "#{question}: #{answer}"
+end
 		
 
 
