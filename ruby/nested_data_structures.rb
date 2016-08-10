bbe2340621a3d03ad370166078409e2c51bd8ef9
Warner Nielsen <bwn888@gmail.com>
2016-08-10 09:23:47 -0600

    Add another data access example

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -5,42 +5,55 @@ classroom = {
 		name: "Teacher's Pets",
 		desk_count: {
 			total_desks: 6,
-			desks_available: 3,
+			desks_taken: {
+				number_desks_taken: 2,
+				desk_contents: [
+					"Books",
+					"Pencils",
+					"Pens",
+					"Calulators",
+					"Apples"
+				],
+			},
+			desks_available: 4
 		},
-		desk_contents: [
-			"Books",
-			"Pencils",
-			"Pens",
-			"Calulators",
-			"Apples",
-		],
 	},
 	middle_of_class: {
 		name: "Keeping Low",
 		desk_count: {
 			total_desks: 6,
-			desks_available: 2,
+			desks_taken: {
+				number_desks_taken: 3,
+				desk_contents: [
+					"Books",
+					"Pens",
+					"Rubberbands"
+				],
+			},
+			desks_available: 3
 		},
-		desk_contents: [
-			"Books",
-			"Pens",
-			"Rubberbands",
-		]
 	},
 	back_of_class: {
 		name: "Slackers",
 		desk_count: {
 			total_desks: 6,
-			desks_available: 0,
-		},
-		desk_contents: ["Empty"]
+			desks_taken: {
+				number_desks_taken: 6,
+				desk_contents: ["Empty"],
+			},
+			desks_available: 0
+		}
 	}
 }
 
-p classroom[:front_of_class][:desk_contents]
+p classroom[:front_of_class][:desk_count][:desks_taken][:desk_contents]
+p classroom[:front_of_class][:desk_count][:desks_taken][:desk_contents].sort_by{|k, v|}.reverse
 p classroom[:back_of_class][:name]
-p classroom[:front_of_class]
-
+p classroom[:back_of_class][:desk_count][:desks_taken][:desk_contents]
+p classroom[:middle_of_class][:name]
+p classroom[:middle_of_class][:desk_count][:desks_taken][:desk_contents]
+p classroom[:back_of_class][:desk_count][:desks_taken][:number_desks_taken]
+p classroom[:front_of_class][:desk_count][:desks_taken][:desk_contents][2]
 
 
 
