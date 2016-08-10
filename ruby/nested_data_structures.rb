b355b61527f85b984fd4ae267b36bf663907f22a
Warner Nielsen <bwn888@gmail.com>
2016-08-09 18:17:44 -0600

    Add nested branches in main branches

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,27 +2,47 @@
 
 classroom = {
 	front_of_class: {
+		name: "Teacher's Pets",
 		desk_count: {
 			total_desks: 6,
-			desks_available: 3
+			desks_available: 3,
 		},
 		desk_contents: [
-			"empty",
-			"full"
+			"Books",
+			"Pencils",
+			"Pens",
+			"Calulators",
+			"Apples",
 		],
 	},
 	middle_of_class: {
+		name: "Keeping Low",
 		desk_count: {
 			total_desks: 6,
-			desks_available: 2
+			desks_available: 2,
 		},
+		desk_contents: [
+			"Books",
+			"Pens",
+			"Rubberbands",
+		]
 	},
 	back_of_class: {
+		name: "Slackers",
 		desk_count: {
 			total_desks: 6,
-			desks_available: 0
-		}
+			desks_available: 0,
+		},
+		desk_contents: ["Empty"]
 	}
 }
 
 p classroom[:front_of_class][:desk_contents]
+p classroom[:back_of_class][:name]
+p classroom[:front_of_class]
+
+
+
+
+
+
