d1359420d1692a3444ab62a0b2c7423ab9af0391
Warner Nielsen <bwn888@gmail.com>
2016-08-11 12:24:20 -0600

    Redo letter_change method, not working properly, save tests as comments at end of file

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -12,19 +12,54 @@ def name_swap(name)
 	name = name.join(' ')
 end
 
-def vowel_pull(name_string)
-	index = 0
-	name_string = name_string.downcase
-	name_string = name_string.split('')
-	vowels = name_string.select { |vowel| vowel =~ /[aeiou]/}
+def vowel_change(name_vowels)
+	name_vowels = name_vowels.downcase
+	name_vowels = name_vowels.split('')
+	vowels = name_vowels.select { |vowel| vowel =~ /[aeiou]/}
+end
+
+def letter_change(name_letters)
+	
 end
 
 #p name_swap("Warner Nielsen")
-p vowel_pull("Eric Estrada")
-#p vowel_pull(name_swap("Warner Nielsen"))
+p vowel_change("Warner Nielsen")
+#p letter_change("Warner Nielsen")
+#p vowel_change(name_swap("Warner Nielsen"))
 
 
 
 
-#name_string.each { |letter| letter.next }
-#name_string.join('')
+
+
+# Gave an error but prints name and letters once
+#i = 0
+#	while i < name_letters.length
+#		if name_letters[i] == " "
+#			i += 1
+#		else
+#			p name_letters = name_letters.split('')
+#			p name_letters = name_letters.map! { |letter| letter.next }
+#			p letters = name_letters.join('')
+#				i +=1
+#		end
+#	end
+#	letters
+
+
+# Prints name as many times as there are letters in the index, does switch letters
+#i = 0
+#	name_letters = name_letters.split("")
+#	name_length = name_letters.length
+#		while i < name_length
+#			if name_letters[i] == " "
+#				i += 1
+#			else
+#				p name_letters = name_letters.map! { |letter| letter.next }
+#				p name_join = name_letters.join("")
+#					i += 1
+#			end
+#		end
+
+
+
