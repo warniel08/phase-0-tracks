d8a6609bc1db4f92adfa68e1110ea661f004006e
Warner Nielsen <bwn888@gmail.com>
2016-08-12 00:39:18 -0600

    Add driver code to ask for name to create alias, put in loop to keep asking for more names, break when word 'quit' is typed

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -10,11 +10,14 @@
 # => letter if there is a "u" swap that for "a", if there is a "z", swap that for "b", 
 # => if there is a " ", leave it
 
+# this method splits a name and reverses it
 def name_swap(name)
 	name = name.split(" ")
 	name = name.reverse
 end
 
+# => this method takes vowels and consonants and shifts the array one to the left 
+# to advance the index of the letter in the array
 def name_change(name_letters)
 	vowels = ["a", "e", "i", "o", "u"]
 	consonants = %w[b c d f g h j k l m n p q r s t v w x y z]
@@ -40,25 +43,28 @@ def name_change(name_letters)
 	letters_change.join
 end
 
+# driver code
 puts "\nThe Great Alias Maker"
 
-alias_name = ""
-
+# code assumes that you enter two names separated by a space
+# loop to keep asking if you want to enter a new name, otherwise 'quit' to end
 loop do 
-	print "(Type 'quit' when finished) Please enter a name you would like to create an alias for: " 
+	puts "\nTo create an alias, please enter a first and last name:"
+	print "(Enter 'quit' to end):> " 
 		alias_name = gets.chomp
-		if alias_name
-			alias_name = name_swap(name_change(alias_name))
-			first_name = alias_name[0].capitalize
-			last_name = alias_name[1].capitalize
-			full_name = first_name + " " + last_name
-			puts "Your new alias is #{full_name}."
-			break
-		elsif alias_name == "quit"
-			break
-		else
-			puts "Sorry please try again!"
-	end
+			if alias_name == "quit"
+				break
+			# if you accidentally hit enter without entering anything
+			elsif alias_name == "" 
+				puts "Sorry please try again!"
+			else original_name = alias_name
+				original_name = name_swap(name_change(alias_name))
+				first_name = original_name[0].capitalize
+				last_name = original_name[1].capitalize
+				full_name = first_name + " " + last_name
+				puts "\nYour new alias is #{full_name}"
+				puts "Your old name is #{alias_name}"
+			end
 end
 
 
