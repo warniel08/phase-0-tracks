e397bd87dfa65b54bf3beaad36dd473709310c0e
Warner Nielsen <bwn888@gmail.com>
2016-08-16 16:20:02 -0600

    Add comments to puppy class like characteristics and behavior

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# Puppy class
+
+# charecteristics:
+	# fur color: tan
+	# eye color: dark brown
+	# eye size: big
+	# tongue: wet
+	# distinguishing characteristicc: dark patch of fur around left eye
+	
+# behavior
+	# loves cuddling
+	# runs around
+	# mischevious
+	# licks everything
+
+
