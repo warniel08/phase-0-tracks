ed67565aff231f4fc4abaca38f9134acd6e06845
Warner Nielsen <bwn888@gmail.com>
2016-08-16 13:36:35 -0600

    Add puppy_methods.rb to 6.2-instance branch, worked on this with Jeff Mendiola

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,5 @@
+#_____ Release 0 & 1 __________________________________________________________
+
 class Puppy
 
   def fetch(toy)
@@ -5,5 +7,71 @@ class Puppy
     toy
   end
 
+  def speak(number)
+    number.times { print "Woof! " }
+  end
+
+  def roll_over
+    puts "\n*rolls over*"
+  end
+
+  def dog_years(human_years)
+    human_years * 7
+  end
+
+  def shake_hand
+    puts "*shakes hand*"
+  end
+
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
+
+end
+
+Spot = Puppy.new
+
+Spot.fetch("ball")
+Spot.speak(5)
+Spot.roll_over
+puts Spot.dog_years(5)
+Spot.shake_hand
+
+#_____ Release 2 ______________________________________________________________
+
+class FootballPlayer
+
+  def pass(yards)
+    puts "Passes ball #{yards} yards to teammate."
+    yards
+  end
+
+  def initialize
+    puts "Initializing new football player instance..."
+  end
+
+  def greeting(name)
+    puts "Hi my name is #{name}."
+  end
+
+end
+
+Joe = FootballPlayer.new
+
+Joe.pass(10)
+Joe.greeting("Joe Riches")
+
+# Question 1 & 2
+football_players = Array.new
+50.times do |player|
+  player = FootballPlayer.new
+  football_players << player
+end
+
+# Question 3
+counter = 0
+football_players.each do |player|
+  player.pass(10)
+  player.greeting("Joe #{counter += 1}")
 end
 
