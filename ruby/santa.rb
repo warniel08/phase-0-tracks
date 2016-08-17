e922234b3a143794b82fc855768460b5598cd8be
Warner Nielsen <bwn888@gmail.com>
2016-08-17 16:21:10 -0600

    Add getters setters, driver code to make sure they work, also added attr

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -19,51 +19,69 @@ class Santa
 		@age = 0
 	end
 
-	def celebrate_birthday(age)
-		age = @age+1
+	#setter methods
+	def celebrate_birthday=(age_increase)
+		@age = age_increase + 1
 	end
 
-	def get_mad_at(reindeer)
-		@reindeer_ranking.index do |x| 
-			if x == "reindeer"
-				
-			end
+	def get_mad_at=(reindeer)
+		index_0 = "Rudolph"
+		index_1 = "Dasher"
+		index_2 = "Dancer"
+		index_3 = "Prancer"
+		index_4 = "Vixen"
+		index_5 = "Comet"
+		index_6 = "Cupid"
+		index_7 = "Donner"
+		index_8 = "Blitzen"
+
+		if reindeer == "Rudolph"
+			@reindeer_ranking.insert(8, @reindeer_ranking.delete_at(0))
+		elsif reindeer == "Dasher"
+			@reindeer_ranking.insert(8, @reindeer_ranking.delete_at(1))
+		elsif reindeer == "Dancer"
+			@reindeer_ranking.insert(8, @reindeer_ranking.delete_at(2))
+		elsif reindeer == "Prancer"
+			@reindeer_ranking.insert(8, @reindeer_ranking.delete_at(3))
+		elsif reindeer == "Vixen"
+			@reindeer_ranking.insert(8, @reindeer_ranking.delete_at(4))
+		elsif reindeer == "Comet"
+			@reindeer_ranking.insert(8, @reindeer_ranking.delete_at(5))
+		elsif reindeer == "Cupid"
+			@reindeer_ranking.insert(8, @reindeer_ranking.delete_at(6))
+		elsif reindeer == "Donner"
+			@reindeer_ranking.insert(8, @reindeer_ranking.delete_at(7))
+		elsif reindeer == "Blitzen"
+			@reindeer_ranking.insert(8, @reindeer_ranking.delete_at(8))
+		end
 	end
 
-	def gender=(new_gender)
-		@gender = new_gender
-	end
+	attr_accessor :gender
 
 	#getter methods
-	def age
-		@age
-	end
+	attr_reader :age, :ethnicity
 
-	def ethnicity
-		@ethnicity
-	end
 end
 
-# new_Santa = Santa.new
-# new_Santa.speak
-# new_Santa.eat_milk_and_cookies("chocolate chip")
+new_Santa = Santa.new("x", "y")
+new_Santa.speak
+new_Santa.eat_milk_and_cookies("chocolate chip")
 	
-# santas = []
-# santas << Santa.new("male", "American")
-# santas << Santa.new("female", "alien")
-# santas << Santa.new("none", "unicorn")
-# santas << Santa.new("agender", "white")
-# santas << Santa.new("N/A", "N/A")
+santas = []
+santas << Santa.new("male", "American")
+santas << Santa.new("female", "alien")
+santas << Santa.new("none", "unicorn")
+santas << Santa.new("agender", "Canadian")
+santas << Santa.new("N/A", "N/A")
 
-
-#test = Santa.new("male", "alien")
-#p "#{test.age} and #{test.ethnicity}"
-
-test = Santa.new("none", "unicorn")
-x = test.get_mad_at("Dancer")
-p x
-
-
-
-
-
+puts 
+test = Santa.new("none", "falcon")
+p test
+test.gender = "male"
+p test
+puts "#{test.ethnicity}"
+test_age = test.celebrate_birthday = 2
+p test
+puts "#{test.age}"
+x = test.get_mad_at=("Vixen")
+p test
