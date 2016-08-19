2ddbc5e427fa7c52366ff278dd9056b88598b161
Warner Nielsen <bwn888@gmail.com>
2016-08-18 21:20:20 -0600

    Add pseudocode for description of class

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,2 +1,29 @@
 # 6.4 Solo Challenge
 # Warner Nielsen
+
+# No matter what you design, your class must have:
+
+# 1. at least three attributes (example: age) (using at least two data types) whose values will vary from instance to instance 
+# at least three methods, at least one of which takes an argument
+# You're welcome to be creative about these: a dress can have a swish method, a soccer ball can have an inflatedness attribute, and ... we hope it's obvious what a fire-breathing dragon can do, but if not, you'll figure it out.
+
+# class that represents 7 dwarves called Dwarf
+# it will contain the attributes of :age, :name, :working(boolean that states true or false if they are working or not)
+# initialize each new Dwarf with these three attributes
+
+# 3 methods:
+
+# method_one(argument)
+# => input:
+# => steps:
+# => output:
+
+# method_two
+# => input:
+# => steps:
+# => output:
+
+# method_three
+# => input:
+# => steps:
+# => output:
