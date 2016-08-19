62a747284d65c6454ebabbba5d91b2e3cf9e7a99
Warner Nielsen <bwn888@gmail.com>
2016-08-19 12:06:09 -0600

    Add driver code to create user interaction, gather data from user, test that code works

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -3,6 +3,10 @@
 # Warner Nielsen
 #--------------------------------------------------------
 
+#--------------------------------------------------------
+# Pseudocode
+#--------------------------------------------------------
+
 # Class that represents 7 dwarves called Dwarf
 # it will contain the attributes of :age, :name, :working(random boolean that states true or false if they are working or not)
 # initialize each new Dwarf with these three attributes
@@ -101,18 +105,73 @@ class Dwarf
 
 end
 
-out_working = [true, false].sample
-dwarves_names = ["Doc", "Grumpy", "Happy", "Sleepy", "Bashful", "Sneezy", "Dopey"].sample
-age = rand(40..150)
-num_loads = rand(0..14)
-num_hours = rand(0..24)
-new_dwarf = Dwarf.new(age, dwarves_names, out_working)
-new_dwarf.loads_mined(num_loads)
-new_dwarf.hours_worked(num_hours)
-new_dwarf.whistle
-new_dwarf.name = "The Dwarf Formerly Known as #{new_dwarf.name}"
-new_dwarf.out_working = "out sourced his work"
-puts "#{new_dwarf.name} is still #{new_dwarf.age} but has #{new_dwarf.out_working}."
+# Your user should be allowed to create as many instances of your class as they like.
+# Prompt the user for each attribute, and don't forget to convert their input to the appropriate data type. Store these class instances in an array.
+# When the user indicates that they are finished creating instances, loop through the array and print out the attributes of each instance as a confirmation message of what was created.
+
+#--------------------------------------------------------
+# 6.4 Driver code
+#--------------------------------------------------------
+
+puts "\nWelcome to the Dwarf Generator"
+puts "\nI will prompt you to input some information"
+puts "into the dwarf generator. Have fun!"
+
+puts "\nType 'done' to let me know when you are finished with the Dwarf Generator."
+puts "Hit 'Enter' if you would like to continue..."
+user_input = gets.chomp
+
+until user_input == 'done'
+	dwarves_names = ["Doc", "Grumpy", "Happy", "Sleepy", "Bashful", "Sneezy", "Dopey"]
+	puts "\n#{dwarves_names.join(" ")}"
+	puts "\nI will randomly assign your dwarf a name."
+	puts "Your dwarf is #{user_name = dwarves_names.sample}. Hello #{user_name}!"
+	
+	print "How old is #{user_name}? "
+	user_age = gets.chomp
+	user_age = user_age.to_i
+	puts "Wow, #{user_name} is #{user_age}. Good for him."
+
+	print "\nHow many human hours did #{user_name} work today? (Dwarves multiply human hours \nby 7 hours when they calculate how many hours they've worked) "
+	user_hours = gets.chomp
+	user_hours = user_hours.to_i
+	puts "#{user_name} worked #{user_hours} human hours today."
+
+	print "\nHow many loads of jewels has #{user_name} mined today? "
+	user_number_of_loads = gets.chomp
+	user_number_of_loads = user_number_of_loads.to_i
+	puts "#{user_name} has mined #{user_number_of_loads} loads today."
+
+	print "\nIs your dwarf still working?"
+	user_still_working = gets.chomp
+		if user_still_working == "yes"
+			user_still_working = true
+			p user_still_working
+		else
+			user_still_working = false
+		end
+
+	puts "\nType 'done' to let me know when you are finished with the Dwarf Generator."
+	puts "Hit 'Enter' if you would like to continue..."
+	user_input = gets.chomp
+end
+
+# Driver code
+# Variables test
+# out_working = [true, false].sample
+# dwarves_names = ["Doc", "Grumpy", "Happy", "Sleepy", "Bashful", "Sneezy", "Dopey"].sample
+# age = rand(40..150)
+# num_loads = rand(0..14)
+# num_hours = rand(0..24)
+
+# New instance test
+# new_dwarf = Dwarf.new(age, dwarves_names, out_working)
+# new_dwarf.loads_mined(num_loads)
+# new_dwarf.hours_worked(num_hours)
+# new_dwarf.whistle
+# new_dwarf.name = "The Dwarf Formerly Known as #{new_dwarf.name}"
+# new_dwarf.out_working = "out sourced his work"
+# puts "#{new_dwarf.name} is still #{new_dwarf.age} but has #{new_dwarf.out_working}."
 
 
 
