fd6ded08ff8b3a8822e05a84b57bc1c436596ed0
Warner Nielsen <bwn888@gmail.com>
2016-08-18 21:36:49 -0600

    Add more pseudocode to describe methods

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -3,13 +3,15 @@
 
 # No matter what you design, your class must have:
 
-# 1. at least three attributes (example: age) (using at least two data types) whose values will vary from instance to instance 
 # at least three methods, at least one of which takes an argument
 # You're welcome to be creative about these: a dress can have a swish method, a soccer ball can have an inflatedness attribute, and ... we hope it's obvious what a fire-breathing dragon can do, but if not, you'll figure it out.
 
-# class that represents 7 dwarves called Dwarf
-# it will contain the attributes of :age, :name, :working(boolean that states true or false if they are working or not)
+# Class that represents 7 dwarves called Dwarf
+# it will contain the attributes of :age, :name, :working(random boolean that states true or false if they are working or not)
 # initialize each new Dwarf with these three attributes
+# => (save this bit of code when I need it: -->random_boolean = [true, false].sample)
+# => just create 7 different dwarves, make an array in driver code of each dwarves name
+# => randomize age in driver code so each dwarves age will be different 
 
 # 3 methods:
 
@@ -18,10 +20,12 @@
 # => steps:
 # => output:
 
-# method_two
-# => input:
+# method_two is for hours_worked(argument of hours_integer)
+# => input: integer
 # => steps:
-# => output:
+			# => take hours integer and multiply by 7
+			# => PRINT the dwarf name and how many dwarf hours he worked today
+# => output: string
 
 # method_three
 # => input:
