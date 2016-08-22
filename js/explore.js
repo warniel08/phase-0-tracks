18da08d7cba0e5a9e6470579ea33412a5e675a7a
Warner Nielsen <bwn888@gmail.com>
2016-08-22 15:37:41 -0600

    Add code to reverse a string

---
+++ js/explore.js
@@ -0,0 +1,15 @@
+//Write a reverse function in js
+
+//Input: string
+//Steps:
+//	Create an empty string and a string with the word in it
+// Take the last latter of the string and put it in the new, formerly empty
+//string
+//Output: reversed string
+
+str = "hello";
+reversedStr = "";
+
+for (var i=0; i > str.length; i--) {
+	console.log(str.lastIndexOf(i));
+	}
