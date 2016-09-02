6c0fe108e17d0a6b9b684e67b07570b6b3af44c2
Warner Nielsen <bwn888@gmail.com>
2016-09-02 14:47:55 -0600

    Add methods to create tool and garage locations, add driver code to store user input

--- databases/week_8_combine/tools_locator.rb
+++ databases/week_8_combine/tools_locator.rb
@@ -1,16 +1,24 @@
 require 'sqlite3'
 
+#----------------------------------------------
+# Create Database
+#----------------------------------------------
+
 tools_db = SQLite3::Database.new("tools_locator.db")
 tools_db.results_as_hash = true
 
-create_table_cmd = <<-SQL
+#----------------------------------------------
+# Create tables in database then execute 
+#----------------------------------------------
+
+create_tools_table = <<-SQL
  CREATE TABLE IF NOT EXISTS Tools(
  	ID INTEGER PRIMARY KEY,
-  Name VARCHAR(255),
+  Name VARCHAR(255)
  );
 SQL
 
-create_table_cmd = <<-SQL
+create_garage_table = <<-SQL
 	CREATE TABLE IF NOT EXISTS Garage(
   Location VARCHAR(255),
   Tool_ID INT,
@@ -18,7 +26,7 @@ create_table_cmd = <<-SQL
  );
 SQL
 
-create_table_cmd = <<-SQL
+create_shed_table = <<-SQL
 	CREATE TABLE IF NOT EXISTS Shed(
   Location VARCHAR(255),
   Tool_ID INT,
@@ -26,4 +34,40 @@ create_table_cmd = <<-SQL
  );
 SQL
 
-tools_db.execute(create_table_cmd)
+tools_db.execute(create_tools_table)
+tools_db.execute(create_garage_table)
+tools_db.execute(create_shed_table)
+
+#----------------------------------------------
+# Methods
+#----------------------------------------------
+
+def create_tool(tools_db, name)
+	tools_db.execute("INSERT INTO Tools (name) VALUES (?)", [name])
+end
+
+def create_garage_location(tools_db, location)
+	tools_db.execute("INSERT INTO Garage (location) VALUES (?)", [location])
+end
+
+
+#----------------------------------------------
+# Driver code to ask user for information
+#----------------------------------------------
+
+print "What tool would you like to add to your inventory? "
+tool_user_input = gets.chomp
+print "Where is the tool located (shed/garage)? "
+location_picker = gets.chomp
+if location_picker == "garage"
+	location_garage_input = gets.chomp
+elsif location_picker == "shed"
+	
+
+create_tool(tools_db, tool_user_input)
+create_garage_location(tools_db, location_garage_input)
+
+
+
+
+
