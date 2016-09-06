f18b682f342c8ce3dd4598a22f66d71ff6f7bde8
Warner Nielsen <bwn888@gmail.com>
2016-09-06 11:34:14 -0600

    Add orca jpeg, update js script file to run event listener and js styling for html

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,14 @@
+console.log("Orca script is running")
+
+var image = document.getElementsByTagName('img');
+
+var image = image[0];
+
+image.style.border = "2px solid red";
+
+
+function undoBorderStyle(event) {
+	event.target.style.border = null;
+}
+
+image.addEventListener("click", undoBorderStyle)
