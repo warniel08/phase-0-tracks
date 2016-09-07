964d17efa6c4ff2e25e75534758f7bd5ee7cfae2
Warner Nielsen <bwn888@gmail.com>
2016-09-07 12:56:53 -0600

    Add location erb and add a route that only pulls up students attending SF campus

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -17,6 +17,11 @@ get '/students/new' do
   erb :new_student
 end
 
+get '/students/location' do
+	@students = db.execute("SELECT * FROM students WHERE campus='SF'")
+	erb :location
+end	
+
 # create new students via
 # a form
 post '/students' do
